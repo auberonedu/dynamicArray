275dcf0e01cc0e5bc587059a00c410a0299a87e5
HumaGitGud <khomidov.huma@student.greenriver.edu>
2025-01-14 14:19:30 -0800

    In progress of implementing remove and size methods

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -5,4 +5,17 @@ public class DynamicStringList implements StringList{
     public String get(int index) {
         return dynamicArray[index];
     }
+
+    public void set(int index, String value) {
+        dynamicArray[index] = value;
+    }
+
+    public String remove(int index) {
+        dynamicArray[index] = null;
+        return dynamicArray[index];
+    }
+
+    public int size() {
+        return dynamicArray.length;
+    }
 }
