33006d57d0ab8636934364832608a229923cbf50
f3liz <felixchen14@gmail.com>
2025-01-14 14:09:28 -0800

    created private String array and implemented get method

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,8 @@
-public class DynamicStringList {
+public class DynamicStringList implements StringList{
     
+    private String[] dynamicArray = new String[10];
+
+    public String get(int index) {
+        return dynamicArray[index];
+    }
 }
