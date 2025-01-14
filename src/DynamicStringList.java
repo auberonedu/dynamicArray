78e229897d1f7b6890c9f9d78d716e4c71301ab9
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-14 15:34:19 -0800

    Added public methods for each section from StringList interface

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,29 @@
-public class DynamicStringList {
-    
+public class DynamicStringList implements StringList {
+  
+    private String[] info;
+    private int size;
+
+    public String get(int index) {
+        return null;
+    }
+
+    public void set(int index, String value) {
+
+    }
+
+    public void add(String value) {
+
+    }
+
+    public String remove(int index) {
+        return null;
+    }
+   
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return null;
+    }
 }
