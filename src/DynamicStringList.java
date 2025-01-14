7f387ba847a52619276cd11be4879c8df305c3ce
dannymccarragher <dannymccarragher@gmail.com>
2025-01-14 14:44:53 -0800

    First attempt on get method

---
+++ src/DynamicStringList.java
@@ -0,0 +1,55 @@
+public class DynamicStringList implements StringList {
+
+    private String myArr[];
+    private int size;
+
+    public void StringList() {
+        myArr = new String[10];
+        size = 0;
+    };
+    
+    @Override
+  public String get(int index) {
+
+
+    if(index < 0 || index >= size()){
+        throw new IndexOutOfBoundsException("Index is out of range");
+        }
+
+        return myArr[index]; 
+    }
+
+
+    @Override
+    public void set(int index, String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public void add(String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public String remove(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public int capacity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'capacity'");
+    }
+
+
+    
+}
