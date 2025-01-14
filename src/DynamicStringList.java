1e84b947bba3d8fa587aeb7f9880f5fd9dec4d9f
alstondsouza1 <alstondsouza35@gmail.com>
2025-01-14 14:25:21 -0800

    added DynamicStringList constructor

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,14 @@
-public class DynamicStringList {
+public class DynamicStringList implements StringList {
+    private String[] array;
+    private int size;
+
+    // constructor
+    public DynamicStringList(int initialCapacity) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Initial capacity must be greather than 0");
+        }
+        array = new String[initialCapacity];
+        size = 0;
+    }
     
 }
