a946b4cb2064a81b4751d0fbf80c7775824eeb13
Z1springer <zdspringer99@gmail.com>
2025-01-14 14:20:15 -0800

    Finished get method in DynamicStringList.java

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,26 +1,32 @@
-public class DynamicStringList implements StringList{
-    
-    public String get(int index){
+public class DynamicStringList implements StringList {
+
+    String[] array = new String[5];
+
+    public String get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return array[index];
+    }
+
+    public void set(int index, String value) {
+
+    }
+
+    public void add(String value) {
+
+    }
+
+    public String remove(int index) {
         return "";
     }
 
-    public void set(int index, String value){
-
-    }
-
-    public void add(String value){
-
-    }
-
-    public String remove(int index){
-        return "";
-    }
-
-    public int size(){
+    public int size() {
         return 0;
     }
 
-    public int capacity(){
+    public int capacity() {
         return 0;
     }
 }
