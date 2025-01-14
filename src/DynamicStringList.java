68c5b9f8b217b54d9aa9c4f6a0db17afa36cfc48
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-01-14 14:21:14 -0800

    Added methods to class

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -2,4 +2,34 @@ public class DynamicStringList implements StringList {
     public DynamicStringList (int length){
         String[] arr = new String[length];
     }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(int index, String value) {
+
+    }
+
+    @Override
+    public void add(String value) {
+
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
 }
