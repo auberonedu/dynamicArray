84d3304dc167a132f5820139108e025dfbfba114
f3liz <felixchen14@gmail.com>
2025-01-14 14:18:22 -0800

    implemented set and add method

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,8 +1,18 @@
 public class DynamicStringList implements StringList{
     
     private String[] dynamicArray = new String[10];
+    private int counter = 0;
 
     public String get(int index) {
         return dynamicArray[index];
+    }
+
+    public void set(int index, String value) {
+        dynamicArray[index] = value;
+    }
+
+    public void add(String value) {
+        dynamicArray[counter] = value;
+        counter++;
     }
 }
