e22ecbeba29491d892e35393d14ea4be7e7c1631
kaalid arare <jaliddd11@gmail.com>
2025-01-14 14:36:24 -0800

    added a value of a specific index

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -10,5 +10,14 @@ public class DynamicStringList implements StringList {
         array = new String[initialCapacity];
         size = 0;
     }
-    
+
+    // get a value of a specific index
+    @Override
+    public String get(int index) {
+        if(index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index out of bounderies: " + index);
+        }
+        return array[index];
+    }   
 }
+
