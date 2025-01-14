7ac4b3d7f207096c3260e8ae5482d9d5e3fa65c6
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-01-14 14:18:29 -0800

    fleshed out class with empty methods

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,26 @@
 public class DynamicStringList implements StringList {
-   
+
+   public String get(int index) {
+    return String stringy(index);
+   }
+
+   public String set(int index, String value) {
+    this value = value;
+   }
+
+   public void add(String value) {
+
+   }
+
+   public String remove(int index) {
+
+   }
+
+   public int size() {
+
+   }
+
+   public int capacity() {
+    
+   }
 }
