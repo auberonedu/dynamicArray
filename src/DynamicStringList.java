a4d6a2fcb9c8718d98c99baa22e0cdc6ef3bb1e4
Kimberly Mageary <kimberlymageary@gmail.com>
2025-01-14 14:37:00 -0800

    Implemented the add method

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,7 +1,16 @@
 public class DynamicStringList implements StringList {
 
-    String[] array = new String[5];
+    private String[] array = new String[5];
+    private int count = 0;
 
+    /**
+   * Retrieves the string at the specified index in the list.
+   *
+   * @param index the index of the string to retrieve.
+   * @return the string at the specified index.
+   * @throws IndexOutOfBoundsException if the index is out of range (index < 0 or
+   *                                   index >= size()).
+   */
     public String get(int index) {
         if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException();
@@ -10,12 +19,38 @@ public class DynamicStringList implements StringList {
         return array[index];
     }
 
+    /**
+   * Replaces the string at the specified index with the given value.
+   *
+   * @param index the index of the string to replace.
+   * @param value the new value to set at the specified index.
+   * @throws IndexOutOfBoundsException if the index is out of range (index < 0 or
+   *                                   index >= size()).
+   */
     public void set(int index, String value) {
-
+        if(index < 0 || index >= size()){
+            throw new IndexOutOfBoundsException();
+        }
+        array[index] = value;
     }
 
+    /**
+   * Adds a new string to the end of the list.
+   *
+   * @param value the string to add to the list.
+   */
     public void add(String value) {
-
+        if(count > size()){
+           String[] placeholder = new String[size() + 1];
+           for(int i =0; i<size()-1; i++){
+            placeholder[i] = array[i];
+           }
+           placeholder[size()+1] = value;
+           array = placeholder;
+           count++;
+        }
+        array[count] = value;
+        count++;
     }
 
     public String remove(int index) {
