528e6c2f40a6777498744365c419c4e27325752f
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-01-14 14:53:51 -0800

    worked on size, capacity, remove

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -2,13 +2,9 @@ public class DynamicStringList implements StringList {
     private String[] strArray;
     private int size; 
 
-    private String[] strArray;
-    private int size;
-
     public DynamicStringList() {
         strArray = new String[10];
         size = 0;
-        
     }
 
    public String get(int index) {
@@ -20,18 +16,21 @@ public class DynamicStringList implements StringList {
    }
 
    public void add(String value) {
-
+    strArray[size] = value;
+    size++;
    }
 
    public String remove(int index) {
-
+    String removedString = strArray[index];
+    strArray[index] = null;
+    return removedString;
    }
 
    public int size() {
-
+    return size;
    }
 
    public int capacity() {
-
+    return strArray.length;
    }
 }
