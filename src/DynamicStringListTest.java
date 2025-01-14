d852cceaa53b1c4827475c0085c188f100aa638e
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-01-14 15:18:31 -0800

    Created test outline

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DynamicStringListTest {
+
+    DynamicStringList stringList = new DynamicStringList();
+    stringList.add("the");
+    stringList.add("quick");
+
+    // test cases:
+    // add 11 items
+    // remove from front, end, middle
+    // set at front, end, middle
+    // check size
+    // check capacity
+
+    @Test
+    void 
     
 }
