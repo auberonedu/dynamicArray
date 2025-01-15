99983c213136053a5060aa0abd748eebed63b112
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-01-15 11:30:24 -0800

    Created one Test case, and started a few others.

--- src/testDynamicStringList.java
+++ src/testDynamicStringList.java
@@ -1,8 +1,27 @@
-public class testDynamicStringList {
-    // 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-    @test
-    public void dynamicArra(){
+public class testDynamicStringList {
+    
+    // Test for Adding and Getting the elements
+    @Test
+    public void testAddAndGet(){
+        DynamicStringList list = new DynamicStringList();
+        list.add("Hello");
+        list.add("World");
+        assertEquals("Hello", list.get(0));
+        assertEquals("World", list.get(1));
+    }
+
+    // Test for removing an element
+    @Test
+    public void testRemove() {
+
+    }
+
+    // Test for resizing
+    @Test
+    public void testResize() {
 
     }
 }
