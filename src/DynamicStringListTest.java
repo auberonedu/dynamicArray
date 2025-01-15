a4160100068d0612d03cc99e84592a25616319c2
Kimberly Mageary <kimberlymageary@gmail.com>
2025-01-15 12:01:31 -0800

    Added test method headers

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -3,4 +3,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DynamicStringListTest {
     
+    // Tests the get method. Should return a string at the specified index.
+    @Test
+    void getTest(){
+
+    }
+
+    // Tests the set method. Should replace the string at a specified index.
+    @Test 
+    void setTest(){
+
+    }
+
+    /* Tests the add method. Should tack a new string at the end of the list.
+    Creates a new list to account for size if capacity is reached, also
+    incremenets up by one. */
+    @Test
+    void addTest(){
+
+    }
+
+    // Tests the remove method. Removed a string at the specified index from the list.
+    @Test
+    void removeTest(){
+
+    }
+
+    // Tests the size method. Returns the number of strings currently in the list.
+    @Test
+    void sizeTest(){
+
+    }
+
+    /* Tests the capactity method. Returns the number of string the list can hold
+    before needing to be resized. */
+    @Test
+    void capacityTest(){
+        
+    }
 }
