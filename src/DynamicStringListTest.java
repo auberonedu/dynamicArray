f1a8a1cb0798dca3f3d899fa0c003d4cd835a5c1
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-01-14 17:29:40 -0800

    Added a test case in DynamicStringTest.java

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DynamicStringListTest {
-    
+    @Test
+    public void testAddAndGet() {
+        DynamicStringList word = new DynamicStringList();
+
+        word.add("avocado");
+        word.add("orange");
+        
+        assertEquals("avocado", word.get(3));
+        assertEquals("orange", word.get(1));
+
+    }
 }
