8a2fb20679e419c5b2786308f1733fdf1d944bb9
Danny McCarragher <dannymccarragher@gmail.com>
2025-01-15 10:18:49 -0800

    Completed size and capacity tests

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -7,13 +7,30 @@ public class DynamicStringListTest {
     public void stringListGetTest() {
         DynamicStringList list = new DynamicStringList();
 
-        String[] myArr = {"Red", "Green", "Blue", "Orange"};
+        String[] myArr = { "Red", "Green", "Blue", "Orange" };
 
-        for(String colors : myArr){
+        for (String colors : myArr) {
             list.add(colors);
         }
 
-        assertEquals("Red", list.get(0) );
+        assertEquals("Red", list.get(0));
+    }
+
+    @Test
+    public void stringListSetTest() {
+        DynamicStringList list = new DynamicStringList();
+
+        String[] myArr = { "Red", "Green", "Blue", "Orange" };
+
+        for (String colors : myArr) {
+            list.add(colors);
+        }
+
+        list.set(0, "Yellow");
+
+        // Passes using .equals and ==
+        assertTrue(list.get(0) == "Yellow");
+        assertTrue(list.get(0).equals("Yellow"));
 
     }
 
@@ -21,9 +38,9 @@ public class DynamicStringListTest {
     public void stringListRemoveTest() {
         DynamicStringList list = new DynamicStringList();
 
-        String[] myArr = {"Red", "Green", "Blue", "Orange"};
+        String[] myArr = { "Red", "Green", "Blue", "Orange" };
 
-        for(String colors : myArr){
+        for (String colors : myArr) {
             list.add(colors);
         }
 
@@ -33,15 +50,41 @@ public class DynamicStringListTest {
     @Test
     public void stringListAddTest() {
         DynamicStringList list = new DynamicStringList();
-        String[] myArr = {"Red", "Green", "Blue", "Orange"};
+        String[] myArr = { "Red", "Green", "Blue", "Orange" };
 
-        for(String colors : myArr){
+        for (String colors : myArr) {
             list.add(colors);
         }
 
         list.add("Purple");
 
         assertTrue(list.get(4) == "Purple");
+    }
+
+    @Test
+    public void stringListSizeTest() {
+        DynamicStringList list = new DynamicStringList();
+        String[] myArr = { "Red", "Green", "Blue", "Orange", "", "" };
+
+        for (String colors : myArr) {
+            list.add(colors);
+        }
+        assertEquals(6, list.size());
+        assertNotEquals(9, list.size());
+    }
+
+    @Test
+    public void stringListCapacityTest() {
+        DynamicStringList list = new DynamicStringList();
+        String[] myArr = { "Red", "Green", "Blue", "Orange", "Yellow", "Purple", "Brown", "Black", "White", "Gray" };
+
+        for (String colors : myArr) {
+            list.add(colors);
+        }
+
+        assertEquals(10, list.size());
+        // check that the array has resized correctly
+        assertTrue(list.capacity() >= 10);
     }
 
 }
